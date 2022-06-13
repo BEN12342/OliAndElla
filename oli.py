127200b72bed199db18187d7d6a73750bccb7121
BEN12342 <ben.read310@icloud.com>
2022-06-13 19:37:53 +1000

    first commit

---
+++ oli.py
@@ -0,0 +1,37 @@
+print("Hello")
+
+#data types:
+
+#The three main types are String, Booleans: True or False, numbers
+
+print("1")
+
+#Variables:
+
+olis_name = "oli"
+
+oliAge = 14
+
+is_oli_hot = False
+
+print(oliAge)
+   
+print(olis_name)
+
+print(is_oli_hot)
+
+print("Oli's name is "  + olis_name)
+
+print("Oli's age is" + str(oliAge))
+
+print("ben"*5)
+
+print(4 + 5)
+
+print(7896879879*55796)
+
+print(6/2)
+
+print(687666-75559565976575)
+
+print(90/10)
